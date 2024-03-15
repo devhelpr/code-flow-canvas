e586104cebbef374d10b5401a84c5f61c831e6ca
Maikel van de Lisdonk <maikel@devhelpr.com>
2024-03-15 13:48:42 +0100

    okay ,that was rather stupid

--- apps/vps-web/src/app/gl-app.element.ts
+++ apps/vps-web/src/app/gl-app.element.ts
@@ -136,7 +136,6 @@ export class GLAppElement extends AppElement<GLNodeInfo> {
         isZoomingViaTouch = false;
         if (event.pointerType === 'touch') {
           skipFirstPointerMoveOnTouch = true;
-          return;
         }
         this.isMovingGLCanvas = true;
         if (glcanvas) {
