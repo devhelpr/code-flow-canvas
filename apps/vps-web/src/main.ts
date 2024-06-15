7b1514328f78b4f0c0327e75e1c5872c7e66133f
Maikel van de Lisdonk <maikel@devhelpr.com>
2024-06-15 09:27:51 +0200

    config

--- apps/vps-web/src/main.ts
+++ apps/vps-web/src/main.ts
@@ -1,5 +1,6 @@
 const url = new URL(window.location.href);
 import './userWorker';
+
 if (url.pathname === '/gl') {
   import('./app/gl-app.element').then((module) => {
     new module.GLAppElement('#app-root');
--- libs/media-library/vite.config.ts
+++ libs/media-library/vite.config.ts
@@ -6,7 +6,7 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/media-library',
+  cacheDir: '../../node_modules/.vite/libs/media-library',
 
   plugins: [
     nxViteTsPaths(),
