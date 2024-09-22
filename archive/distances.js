df8b8f76eda96e440a6b8cd3adfff764dd5e207d
Maikel van de Lisdonk <maikel@devhelpr.com>
2024-09-22 11:43:45 +0200

    kmeans converged check improvement

--- archive/distances.js
+++ archive/distances.js
@@ -44,3 +44,34 @@
   });
   return centroids;
 };
+
+(clusters, centroids) => {
+  let newcentroids = [];
+  clusters.forEach((cluster) => {
+    let sumX = 0;
+    let sumY = 0;
+    cluster.forEach((point) => {
+      sumX += point[0];
+      sumY += point[1];
+    });
+    newcentroids.push([sumX / cluster.length, sumY / cluster.length]);
+  });
+  let centroidsfound = [];
+  newcentroids.forEach((newcentroid) => {
+    centroids.forEach((oldcentroid, index) => {
+      const indexWasMatchedForOtherCentrod =
+        centroidsfound.findIndex((centroidIndex) => index === centroidIndex) >=
+        0;
+      if (
+        !indexWasMatchedForOtherCentrod &&
+        oldcentroid[0] === newcentroid[0] &&
+        oldcentroid[1].toFixed(5) === newcentroid[1].toFixed(5)
+      ) {
+        centroidsfound.push(index);
+      }
+    });
+  });
+  const converged = centroidsfound.length === newcentroids.length;
+  console.log('converged', centroidsfound, converged, newcentroids, centroids);
+  return { newcentroids: newcentroids, converged: converged };
+};
