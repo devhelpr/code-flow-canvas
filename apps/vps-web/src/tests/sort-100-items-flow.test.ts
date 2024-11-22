4e06376da7d6e5bc98bd1a8a31008caef15fd6ee
Maikel van de Lisdonk <maikel@devhelpr.com>
2024-11-22 21:39:31 +0100

    UT for sorting 100 random items

---
+++ apps/vps-web/src/tests/sort-100-items-flow.test.ts
@@ -0,0 +1,24 @@
+/**
+ * @vitest-environment node
+ **/
+import { expect, test, vi } from 'vitest';
+import { flow } from './flows/sort-100-items-flow';
+import { RuntimeFlowEngine } from '@devhelpr/web-flow-executor';
+
+describe('sort flow', () => {
+  test('runs sort flow', async () => {
+    const flowEngine = new RuntimeFlowEngine();
+    flowEngine.initialize(flow.flows.flow.nodes);
+    const result = await flowEngine.run();
+    console.log('result', result);
+    expect(Array.isArray(result)).toBe(true);
+
+    (result as unknown as any[]).forEach((item, index) => {
+      if (index < result.length - 1) {
+        expect(item <= result[index + 1]).toBe(true);
+      }
+    });
+
+    expect(result.length).toBe(100);
+  });
+});
