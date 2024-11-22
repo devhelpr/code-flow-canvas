9572e408dc898c3cf7432f2270a383205f266c25
Maikel van de Lisdonk <maikel@devhelpr.com>
2024-11-22 09:55:23 +0100

    improve defining background color for canvas (minimizing duplication)

--- libs/visual-programming-system/src/canvas-app/css-classes/canvasapp-css-classes.ts
+++ libs/visual-programming-system/src/canvas-app/css-classes/canvasapp-css-classes.ts
@@ -1,7 +1,6 @@
 export const getCanvasAppCssClasses = () => {
   return {
-    canvasAppClasses: `w-full h-full origin-top-left  flex-auto relative z-10 transition-none transform-gpu`,
-    defaultBackgroundColor: 'bg-slate-800',
+    canvasAppClasses: `w-[0px] h-[0px] origin-top-left  flex-auto relative z-10 transition-none transform-gpu`,
     autoPointerEvents: 'pointer-events-auto',
     defaultNodePortClasses: 'top-0 left-0 origin-center z-[1150]',
   };
