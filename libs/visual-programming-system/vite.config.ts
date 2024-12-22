8e1a7956fee77580da9982e5844557a4e53255a1
Maikel van de Lisdonk <maikel@devhelpr.com>
2024-12-22 16:06:11 +0100

    improved tsconfigs etc.. and use vite for visual-programming-system .. still composite:true issues though (it's not used currently)

--- libs/visual-programming-system/vite.config.ts
+++ libs/visual-programming-system/vite.config.ts
@@ -1,36 +1,62 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
 
-//import viteTsConfigPaths from 'vite-tsconfig-paths';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+import dts from 'vite-plugin-dts';
+import * as path from 'path';
 
 export default defineConfig({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/visual-programming-system',
+
   plugins: [
-    // viteTsConfigPaths({
-    //   root: '../../',
-    // }),
+    dts({
+      entryRoot: 'src',
+      tsConfigFilePath: path.join(__dirname, 'tsconfig.lib.json'),
+      skipDiagnostics: true,
+    }),
+
     nxViteTsPaths(),
   ],
 
   // Uncomment this if you are using workers.
   // worker: {
-  //  plugins: [
-  //    viteTsConfigPaths({
-  //      root: '../../',
-  //    }),
-  //  ],
+  //  plugins: [ nxViteTsPaths() ],
   // },
+
+  // Configuration for building your library.
+  // See: https://vitejs.dev/guide/build.html#library-mode
+  build: {
+    outDir: '../../dist/libs/visual-programming-system',
+    reportCompressedSize: true,
+    commonjsOptions: { transformMixedEsModules: true },
+    lib: {
+      // Could also be a dictionary or array of multiple entry points.
+      entry: 'src/index.ts',
+      name: 'visual-programming-systems',
+      fileName: 'index',
+      // Change this to the formats you want to support.
+      // Don't forget to update your package.json as well.
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      // External packages that should not be bundled into your library.
+      external: [],
+    },
+  },
 
   test: {
     reporters: ['default'],
     coverage: {
-      reportsDirectory: '../../coverage/apps/vps-web',
+      reportsDirectory: '../../coverage/libs/visual-programming-system',
       provider: 'v8',
     },
     globals: true,
-
-    environment: 'jsdom',
-    include: ['./src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    cache: {
+      dir: '../../node_modules/.vitest',
+    },
+    environment: 'node',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     passWithNoTests: true,
   },
 });
