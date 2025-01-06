dcb591562a75ec274e7785397e763765545af1a8
Maikel van de Lisdonk <maikel@devhelpr.com>
2025-01-06 13:06:11 +0100

    ocif 0.2

--- libs/app-canvas/src/app/exporters/ocwg/ocwg-empty-file.ts
+++ libs/app-canvas/src/app/exporters/ocwg/ocwg-empty-file.ts
@@ -1,7 +1,7 @@
 import { OCWGFile } from './ocwg-schema';
 
 export const ocwgEmptyFile: OCWGFile = {
-  ocif: 'https://canvasprotocol.org/ocif/0.1',
+  ocif: 'https://canvasprotocol.org/ocif/0.2',
   nodes: [],
   relations: [],
   resources: [],
